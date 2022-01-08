19ca4d285ce19aa86c84ffaa0671334fa158714e
mperry5th <90285075+mperry5th@users.noreply.github.com>
2022-01-08 12:04:55 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,6 @@
+﻿$("#buttonSend").click(function () {
+    alert("From: " + $("#from").val() +
+        ", Subject: " + $("#subject").val() +
+        ", Message: " + $("#message").val())
+    $("#picGanderson").fadeToggle();
+});
